9e5efca3ad5338251e4832917db4d6504ed75ef8
kdnakt <a.kid.1985@gmail.com>
2021-06-10 08:27:17 +0900

    add image

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { AppFooter } from './AppFooter';
 import { AppHeader } from './AppHeader';
+import { AppImage } from './AppImage';
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <AppImage />
       <AppFooter />
     </div>
   );
---
+++ src/AppImage.tsx
@@ -0,0 +1,10 @@
+
+export const AppImage = () => {
+    return (
+        <h4 style={{textAlign: 'center'}}>
+            <img alt="Crane and Turtle"
+                className="app_image"
+                src="ct.png" />
+        </h4>
+    )
+}

367b2e2eb1eb55d54b6c7b7d5d2c400b131e410a
kdnakt <a.kid.1985@gmail.com>
2021-06-18 08:28:55 +0900

    fix footer position

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,9 +1,9 @@
 
 export const AppFooter = () => {
     return (
-        <h5 style={{textAlign: 'center'}}>
+        <footer className="app_footer" >
             &copy;&nbsp;2021&nbsp;
             <a href="https://twitter.com/kdnakt">kdnakt</a>
-        </h5>
+        </footer>
     )
 }

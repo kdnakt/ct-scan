2e87e604c3021452fbdb634bc69810421a604641
kdnakt <a.kid.1985@gmail.com>
2021-06-24 08:22:16 +0900

    add main test

---
+++ src/components/quiz/QuizMain.test.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react"
+import { QuizMain } from "./QuizMain"
+
+describe('QuizMain', () => {
+    it('should show total heads count', () => {
+        const main = render(<QuizMain quiz={{
+            mode: "easy",
+            data: {
+                craneCount: 2,
+                turtleCount: 3,
+            },
+        }}/>)
+        const totalHeads = main.getByTestId('total-heads')
+        expect(totalHeads).toBe(5)
+    })
+
+    it('should show total legs count', () => {
+        const main = render(<QuizMain quiz={{
+            mode: "easy",
+            data: {
+                craneCount: 2,
+                turtleCount: 3,
+            },
+        }}/>)
+        const totalLegs = main.getByTestId('total-legs')
+        expect(totalLegs).toBe(16)
+    })
+})

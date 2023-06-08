580a03b993161a31eb78e3bd9ac1e7c16e902dde
kdnakt <a.kid.1985@gmail.com>
2023-06-09 01:20:19 +0900

    feat: show current mode

--- src/components/quiz/QuizMain.tsx
+++ src/components/quiz/QuizMain.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react"
 import { Quiz } from "./Quiz"
 import { calcTotalHeadCount, calcTotalLegCount } from "./QuizData"
+import { QuizModes } from "./QuizMode"
 
 export const QuizMain: FC<{quiz: Quiz}> = ({
     quiz,
 }) => {
-    const data = quiz.data
+    const { mode, data } = quiz
+    const label = QuizModes[mode].label
     return (
         <>
+            <h5 style={{textAlign: 'center'}}>
+                <span>モード：{label}</span>
+            </h5>
             <h3 style={{textAlign: 'center'}}>
                 <span>つる&nbsp;と&nbsp;かめ&nbsp;が&nbsp;ぜんぶで&nbsp;</span>
                 <span data-testid="total-heads">{calcTotalHeadCount(data)}</span>
